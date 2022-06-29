5367c5161a20d000c13fd339f683387603f447a9
t <tom@cardtrader.com>
2022-06-29 18:03:22 +0200

    ;

--- src/extension.ts
+++ src/extension.ts
@@ -79,5 +79,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-	console.log("Extension deactivated")
+	console.log("Extension deactivated");
 }
